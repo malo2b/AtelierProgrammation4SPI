9f242924362415b126268298dcfd465d39e4ffc6
Your Name <malo2b>
2021-09-15 12:39:39 +0200

    update

--- ex1.py
+++ ex1.py
@@ -9,7 +9,10 @@ def full_name(chaine:str) -> str:
     Returns:
         str: haine de caractère composée d'un nom et d'un prénom séparé d'un espace modifiée
     """
-    flag = chaine.split(" ")
+    flag = chaine.strip()
+    flag = flag.split(" ")
+    while '' in flag:
+        flag.remove('')
     return flag[0].upper() + " " + flag[1].capitalize()
 
 def saisie_full_name() -> str:
@@ -30,8 +33,36 @@ def saisie_full_name() -> str:
             print("Veuillez faire une saisie valide !")
     return str_saisie
 
-print(
-    full_name(
-        saisie_full_name()
-    )
-)
+# print(
+#     full_name(
+#         saisie_full_name()
+#     )
+# )
+
+def is_mail(chaine:str) -> tuple:
+    """Fonction qui dis si un mail est valide et retourne un code d'erreur sinon
+
+    Args:
+        chaine (str): Chaine a verifier
+
+    Returns:
+        tuple: retourne un tuple composé d'une premiere valeur (1 si valide, 0 sinon)
+                   et code d'erreur sinon
+    """
+    resultat = ()
+    pattern_email = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    chaine = chaine.lower()
+    if re.match(pattern_email,chaine): # Si email valide
+        resultat = (1,0)
+    else:
+        if not re.match("^.*@{1}.*",chaine): # Si @ manquant
+            resultat = (0,2)
+        else:
+            mail_split = chaine.split("@") # Sépare le corp du nom de domaine
+            if not re.match("^[a-zA-Z0-9]{3,}([^ ][a-zA-Z0-9]{3,10})?$",mail_split[0]): # Verification du corp
+               resultat = (0,1)
+            elif re.match("[^.]",mail_split[1]):
+                resultat = (0,4)
+            elif not re.match("^[a-z0-9.-_]{3,15}[.][a-z0-9.-_]{2,10}",mail_split[1]):
+                resultat = (0,3)
+    return resultat
---
+++ testex1.py
@@ -0,0 +1,72 @@
+from ex1 import full_name, is_mail
+
+def test_is_mail() -> bool:
+    """Fonction de test de la fonction is_mail()
+
+    Returns:
+        bool: True si tous les tests sont valides, False sinon
+    """
+    EMAIL_TO_TEST = {
+        "bisgambiglia_paul@univ-corse.fr":(1,0),
+        "bisgambiglia_paul@univ-corse.fr  ":(1,0),
+        "bisgambiglia_paul@academie.univ-corse.fr":(1,0),
+        "bisgambiglia_paul@academie.univ.corse.fr":(1,0),
+        "@univ-corse.fr":(0,1),
+        "ab@univ-corse.fr":(0,1),
+        "bisgambiglia_paulOuniv-corse.fr":(0,2),
+        "bisgambiglia_paul@.fr":(0,3),
+        "bisgambiglia_paul@univ corse.fr":(0,3),
+        "bisgambiglia_paul@univ-corsePOINTfr":(0,4)
+    }
+    nbr_test_valide = 0
+    for value_to_test in EMAIL_TO_TEST.items():
+        str_affichage = "Test : \"{}\", resultat attendu : {}, resultat obtenu : {}, reussit : ".format(value_to_test[0],value_to_test[1],str(is_mail(value_to_test[0])))
+        if value_to_test[1] == is_mail(value_to_test[0]):
+            str_affichage += "Oui"
+            nbr_test_valide += 1
+        else:
+            str_affichage += "Non"
+        print(str_affichage)
+
+    if nbr_test_valide == len(EMAIL_TO_TEST):
+        res = True
+    else:
+        res = False
+    return res
+
+# print (
+#     test_is_mail()
+# )
+
+def test_full_name()-> bool:
+    """Fonction de test de la fonction full_name()
+
+    Returns:
+        bool: True si tous les tests sont valides, False sinon
+    """
+    NAME_TO_TEST = {
+    #     "Girard Malo":"GIRARD Malo",
+    #     "girard malo":"GIRARD Malo",
+    #     "GIRARD Malo":"GIRARD Malo",
+    #     "Girard Malo Michel":"GIRARD Malo",
+        " girard  malo   ":"GIRARD Malo"
+    }
+    nbr_test_valide = 0
+    for value_to_test in NAME_TO_TEST.items():
+        str_affichage = "Test : \"{}\", resultat attendu : {}, resultat obtenu : {}, reussit : ".format(value_to_test[0],value_to_test[1],str(full_name(value_to_test[0])))
+        if value_to_test[1] == full_name(value_to_test[0]):
+            str_affichage += "Oui"
+            nbr_test_valide += 1
+        else:
+            str_affichage += "Non"
+        print(str_affichage)
+
+    if nbr_test_valide == len(NAME_TO_TEST):
+        res = True
+    else:
+        res = False
+    return res
+
+print (
+    test_full_name()
+)
